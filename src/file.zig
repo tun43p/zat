51c362f9a9b779111751e24b3df04ff762630246
tun43p <tun43p@icloud.com>
2026-03-18 20:13:43 +0100

    feat: create tui

--- src/file.zig
+++ src/file.zig
@@ -1,12 +1,6 @@
 const std = @import("std");
 const mime = @import("mime");
 
-const logger = @import("logger.zig").Logger;
-const size = @import("size.zig").Size;
-
-const chars = logger.chars;
-const style = logger.style;
-
 pub const ZatFile = struct {
     path: []const u8,
     name: []const u8,
@@ -49,44 +43,6 @@ pub const ZatFile = struct {
             .line_count = line_count,
             .content = buffer,
         };
-    }
-
-    pub fn print(self: ZatFile) !void {
-        const term_size = size.get(std.fs.File.stdout()) catch |err| {
-            std.debug.print("Error getting terminal size: {s}\n", .{@errorName(err)});
-            return err;
-        };
-
-        if (term_size == null) return error.TerminalSizeNotFound;
-
-        logger.print_pipe(style.red, 5, 1);
-        logger.print(style.cyan, style.bold, "{s}", .{self.name});
-        logger.print_pipe(style.red, 1, 1);
-        logger.print(style.magenta, style.italic, "{s}", .{self.path});
-        logger.print(style.reset, null, "\n", .{});
-
-        for (0..term_size.?.width) |_| {
-            logger.print_row(style.red);
-        }
-
-        var line_num: usize = 1;
-        logger.print(style.red, null, "{d: >4} │ ", .{line_num});
-
-        for (self.content) |byte| {
-            std.debug.print("{c}", .{byte});
-            if (byte == '\n') {
-                line_num += 1;
-                logger.print(style.red, null, "{d: >4} │ ", .{line_num});
-            }
-        }
-
-        logger.print(style.reset, null, "\n\n", .{});
-        logger.print(style.gray, null, "       {s}", .{self.path});
-        logger.print(style.gray, null, " | {s}", .{self.mime});
-        logger.print(style.gray, null, " | {d} lines", .{self.line_count});
-        logger.print(style.gray, null, " | {d} bytes", .{self.size});
-        logger.print(style.gray, null, " | {s}", .{self.encoding});
-        logger.print(style.reset, null, "\n", .{});
     }
 
     pub fn deinit(self: ZatFile, allocator: std.mem.Allocator) void {
--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 
 const ZatFile = @import("file.zig").ZatFile;
+const Tui = @import("tui.zig").Tui;
 
 pub fn main() !void {
     const allocator = std.heap.page_allocator;
@@ -8,5 +9,8 @@ pub fn main() !void {
     const file = try ZatFile.init(allocator, "README.md");
     defer file.deinit(allocator);
 
-    try file.print();
+    var tui = try Tui.init();
+    defer tui.deinit(allocator);
+
+    try tui.render(allocator, file);
 }
