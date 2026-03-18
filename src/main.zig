348ceefc95acfefb158fd1c0509d9009161e9d1e
tun43p <tun43p@icloud.com>
2026-03-18 20:24:29 +0100

    feat: pass file as parameter

--- src/main.zig
+++ src/main.zig
@@ -6,7 +6,17 @@ const Tui = @import("tui.zig").Tui;
 pub fn main() !void {
     const allocator = std.heap.page_allocator;
 
-    const file = try ZatFile.init(allocator, "README.md");
+    var args = try std.process.argsWithAllocator(allocator);
+    defer args.deinit();
+
+    _ = args.skip();
+
+    const file_path = args.next() orelse {
+        std.debug.print("Usage: zat <file>\n", .{});
+        return error.MissingArgument;
+    };
+
+    const file = try ZatFile.init(allocator, file_path);
     defer file.deinit(allocator);
 
     var tui = try Tui.init();
